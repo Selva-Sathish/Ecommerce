9d6a207d5dc80915272110ad391c2bcf2fbdc9bf
selva satheesh <selvasathish@kenshinsystems.com>
2024-11-16 19:12:03 +0530

    Feat: header add

---
+++ frontend/src/Routes/MenuLink.jsx
@@ -0,0 +1,6 @@
+export const Links = [
+    {
+        img: '',
+        descrption : '',
+    }
+]
---
+++ frontend/src/component/HeaderIcons/search.jsx
@@ -0,0 +1,9 @@
+function SearchIcon(){
+    return <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="11.5" cy="11.5" r="9.5" stroke="#1C274C" stroke-width="0.5"/>
+    <path d="M18.5 18.5L22 22" stroke="#1C274C" stroke-width="0.5" stroke-linecap="round"/>
+    </svg>
+}
+
+
+export default SearchIcon;
